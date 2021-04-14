74dadc9178738903e8aa0e3ed2ce4829964f1419
Marc <marcllopis89@gmail.com>
2021-04-14 12:31:27 +0200

    styled

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let results = document.getElementById('results')
 calculate.addEventListener('click', ()=>{
   let value = Number(total.value)
   let solution = nerdamer.solveEquations(['x+((x*15)/100)=y', 'y+((y*25)/100)=z', `x+y+z=${value}`]);
-  console.log(solution);
   results.innerHTML = `
-    <h3>Month 1: ${solution[0][1]}</h3>
-    <h3>Month 2: ${solution[1][1]}</h3>
-    <h3>Month 3: ${solution[2][1]}</h3>
+    <h3>Month 1 -> <span>${Number(solution[0][1]).toFixed(2)} $</span></h3>
+    <h3>Month 2 -> ${Number(solution[1][1]).toFixed(2)} $</h3>
+    <h3>Month 3 -> ${Number(solution[2][1]).toFixed(2)} $</h3>
+    <h3>Total this quarter is ${value} $</h3>
   `
 })
